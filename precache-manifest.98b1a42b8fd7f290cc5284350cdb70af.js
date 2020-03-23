845aebeb152f5bdf57a9c5f6d70d2fc61b761ed0
proftimeru <proftime.ru@gmail.com>
2020-03-23 18:05:48 +0700

    0.1.0

---
+++ precache-manifest.98b1a42b8fd7f290cc5284350cdb70af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b70290b848877973ca538ed4ea58c0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62fb843a1c168e486636",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "bfb5cd90bbb3552ae1a0",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "62fb843a1c168e486636",
+    "url": "/static/js/2.8d8b21cf.chunk.js"
+  },
+  {
+    "revision": "abf0b813b0144a2f8f7c56edfbc053fa",
+    "url": "/static/js/2.8d8b21cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb5cd90bbb3552ae1a0",
+    "url": "/static/js/main.88a7946d.chunk.js"
+  },
+  {
+    "revision": "2f2c4feadb32e02d879d",
+    "url": "/static/js/runtime-main.f72aee86.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
